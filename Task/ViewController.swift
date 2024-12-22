f2d18440618c85cddd160a5ed2e855233f374123
victorgervac <victorgervacio33@gmail.com>
2024-12-21 22:19:40 -0700

    Initial Commit

---
+++ Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task
+//
+//  Created by Victor Gervacio on 12/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TaskTests/TaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTests.swift
+//  TaskTests
+//
+//  Created by Victor Gervacio on 12/21/24.
+//
+
+import Testing
+@testable import Task
+
+struct TaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
